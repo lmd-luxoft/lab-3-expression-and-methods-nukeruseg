75964d54e3d1d993289bb6b695bae3e6b5ac4c75
Григорян Александр <nukeruseg@gmail.com>
2020-12-09 11:38:23 +0300

    refactor

--- XO/Program.cs
+++ XO/Program.cs
@@ -8,9 +8,9 @@ namespace XO
 {
     class Program
     {
-       static char win = '-';
-       static string PlayerName1, PlayerName2;
-       static char[] cells = new char[]{ '-', '-', '-', '-', '-', '-', '-', '-', '-' };
+        static char win = '-';
+        static string[] Players = new string[2];
+        static char[] cells = new char[] { '-', '-', '-', '-', '-', '-', '-', '-', '-' };
 
         static void show_cells()
         {
@@ -24,14 +24,13 @@ namespace XO
             Console.WriteLine("Текущая ситуация (---пустой):");
             Console.WriteLine($"-{cells[0]}-|-{cells[1]}-|-{cells[2]}-");
             Console.WriteLine($"-{cells[3]}-|-{cells[4]}-|-{cells[5]}-");
-            Console.WriteLine($"-{cells[6]}-|-{cells[7]}-|-{cells[8]}-");        
+            Console.WriteLine($"-{cells[6]}-|-{cells[7]}-|-{cells[8]}-");
         }
-        static void make_move(int num)
+        static void make_move(int player)
         {
             string raw_cell;
             int cell;
-            if (num == 1) Console.Write(PlayerName1);
-            else Console.Write(PlayerName2);
+            Console.Write(Players[player]);
             do
             {
                 Console.Write(",введите номер ячейки,сделайте свой ход:");
@@ -49,29 +48,27 @@ namespace XO
                 while (!Int32.TryParse(raw_cell, out cell));
                 Console.WriteLine();
             }
-            if (num == 1) cells[cell - 1] = 'X';
-            else cells[cell - 1] = 'O';
-            
+            cells[cell - 1] = player == 1 ? 'X' : 'O';
         }
         static char check()
         {
-            for (int i = 0; i < 3; i++)
-                if (cells[i * 3] == cells[i * 3 + 1] && cells[i * 3 + 1] == cells[i * 3 + 2])
+            for (int i = 0; i < 3; i++) {
+                bool horRowWin = cells[i * 3] == cells[i * 3 + 1] && cells[i * 3 + 1] == cells[i * 3 + 2];
+                bool vertRowWin = cells[i] == cells[i + 3] && cells[i + 3] == cells[i + 6];
+                bool diagWin = (cells[2] == cells[4] && cells[4] == cells[6]) || (cells[0] == cells[4] && cells[4] == cells[8]);
+                if (horRowWin || vertRowWin || diagWin)
                     return cells[i];
-                else if (cells[i] == cells[i + 3] && cells[i + 3] == cells[i + 6])
-                    return cells[i];
-                else if ((cells[2] == cells[4] && cells[4] == cells[6]) || (cells[0] == cells[4] && cells[4] == cells[8]))
-                    return cells[i];
+            }
             return '-';
         }
 
         static void result()
         {
-            if (win == 'X')
-                Console.WriteLine($"{PlayerName1} вы  выиграли поздравляем {PlayerName2} а вы проиграли...");
-            else if (win == 'O')
-                Console.WriteLine($"{PlayerName2} вы  выиграли поздравляем {PlayerName1} а вы проиграли...");
-
+            var winIdx = win == 'X' ? 0 : 1;
+            var loseIdx = win == 'X' ? 1 : 0;
+            var winner = Players[winIdx];
+            var loser = Players[loseIdx];
+            Console.WriteLine($"{winner} вы  выиграли поздравляем {loser} а вы проиграли...");
         }
 
         static void Main(string[] args)
@@ -79,22 +76,20 @@ namespace XO
             do
             {
                 Console.Write("Введите имя первого игрока : ");
-                PlayerName1 = Console.ReadLine();
+                Players[0] = Console.ReadLine();
 
                 Console.Write("Введите имя второго игрока: ");
-                PlayerName2 = Console.ReadLine();
+                Players[1] = Console.ReadLine();
                 Console.WriteLine();
-            } while (PlayerName1 == PlayerName2);
+            } while (Players[0] == Players[1]);
 
             show_cells();
 
             for (int move = 1; move <= 9; move++)
             {
-                if (move % 2 != 0) make_move(1);
-                else make_move(2);
-
+                var player = move % 2;
+                make_move(player);
                 show_cells();
-
                 if (move >= 5)
                 {
                     win = check();
